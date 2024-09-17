fdfd896d5e68de279da839ddf95ffe861c385715
Ananda Fitra <anandadf@student.ub.ac.id>
2024-09-17 11:32:26 +0700

    add more endpoint

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello	World!');
 });
+
+app.get('/hello-world-2', (req, res) => {
+  res.send('halo dunia!');
+})
 app.listen(port, () => {
   console.log(`App	listening	on	port	${port}`);
 });
